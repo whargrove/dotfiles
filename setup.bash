1ec93034fca2dcb7c9dfaacdf2d5242d09876ccf
Wes Hargrove <whargrove@mac.com>
2014-07-05 11:03:02 -0700

    Finishes setup.bash

--- setup.bash
+++ setup.bash
@@ -1,14 +1,38 @@
 #!/usr/local/bin/bash
 
-# Create ignores
-# .git
-# setup.bash
-# LICENSE
-# README.md
+# Create ignores array because we don't want to symlink these to ~
+ignores=("." ".." ".git" "setup.bash" "LICENSE" "README.md")
+# Create files array to store file names
+files=()
 
-# Loop through all files in .
-  # if ./$file_name matches any ignores
-    # continue loop
-  # if ../$file_name already exists
-    # rename file to $file_name.old
-  # symlink ./$file_name to $../$file_name
+# Include hidden files in glob
+shopt -s dotglob
+
+for entry in *
+do
+  files+=($entry);
+done
+
+# Remove hidden files in glob
+shopt -u dotglob
+
+for f in "${files[@]}"
+do
+  # If $f matches any ignores
+  # Skip this loop iteration
+  for i in "${ignores[@]}"
+  do
+    if [ $f = $i ]; then
+      continue
+    fi
+  done
+
+  # If $f already exists in the user's home directory
+  # Rename the file before creating the symlink
+  if [ -f ~/$f ]; then
+    mv ../$f ../$f.old
+  fi
+
+  # Create symlink: source $f, target: ~/$f
+  ln -s ./$f ~/$f
+done
