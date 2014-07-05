c22817f92b2745ad897836e82672f6d9250d7713
Wes Hargrove <whargrove@mac.com>
2014-07-05 12:36:42 -0700

    Added echoes to stdout; fixed bug where should_be_ignored was not set to true.

--- setup.bash
+++ setup.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "** Getting ready to setup your .dotfiles ..."
+
 # Create ignores array because we don't want to symlink these to ~
 ignores=(".git" "setup.bash" "LICENSE" "README.md")
 # Create files array to store file names
@@ -16,29 +18,38 @@ done
 # Remove hidden files in glob
 shopt -u dotglob
 
+echo "** Starting files loop"
 for f in "${files[@]}"
 do
+  echo "** Checking if file: $f should be ignored ..."
   # If $f matches any ignores
   # Skip this loop iteration
   should_be_ignored=false
   for i in "${ignores[@]}"
   do
     if [ $f = $i ]; then
-      $should_be_ignored=true
+      unset should_be_ignored
+      should_be_ignored=true
+      echo "** File: $f will be ignored"
       break
     fi
   done
 
   if $should_be_ignored ; then
+    echo "** File: $f has been ignored"
     continue
   fi
 
+  echo "** File: $f is not ignored"
+  echo "** Checking if file: $f already exists in ~ and should be backed up ..."
   # If $f already exists in the user's home directory
   # Rename the file before creating the symlink
   if [ -f ~/$f ]; then
-    mv ../$f ../$f.old
+    mv ~/$f ~/$f.old
+    echo "** File: $f already exists in ~ and has been backed up"
   fi
 
+  echo "** Creating symlink for file: $f"
   # Create symlink: source $f, target: ~/$f
   current_dir=$(pwd)
   ln -s $current_dir/$f ~/$f
