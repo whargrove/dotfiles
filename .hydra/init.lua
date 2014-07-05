b37e2e2e8603340e1701cec6ba3a2e01785376e4
Wes Hargrove <whargrove@mac.com>
2014-07-05 13:29:25 -0700

    Adds hydra grid

--- .hydra/init.lua
+++ .hydra/init.lua
@@ -1,46 +1,50 @@
--- open a repl
---   the repl is a Lua prompt; type "print('hello world')"
---   when you're in the repl, type "help" to get started
---   almost all readline functionality works in the repl
-hotkey.bind({"cmd", "ctrl", "alt"}, "R", repl.open)
+require "grid"
 
-hotkey.bind({"cmd", "ctrl"}, "l", movewindow_leftside)
-hotkey.bind({"cmd", "ctrl"}, "r", movewindow_rightside)
+pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
+autolaunch.set(true)
 
--- save the time when updates are checked
-function checkforupdates()
-  updates.check()
-  settings.set('lastcheckedupdates', os.time())
-end
-
--- show a helpful menu
+-- Setup menu
 menu.show(function()
-    local updatetitles = {[true] = "Install Update", [false] = "Check for Update..."}
-    local updatefns = {[true] = updates.install, [false] = checkforupdates}
-    local hasupdate = (updates.newversion ~= nil)
-
     return {
       {title = "Reload Config", fn = hydra.reload},
       {title = "-"},
       {title = "About", fn = hydra.showabout},
-      {title = updatetitles[hasupdate], fn = updatefns[hasupdate]},
       {title = "Quit Hydra", fn = os.exit},
     }
 end)
 
--- show available updates
-local function showupdate()
-  os.execute('open https://github.com/sdegutis/Hydra/releases')
-end
+local mash = {"cmd", "ctrl", "alt"}
+local mashshift = {"cmd", "alt", "shift"}
+local cmdctrl = {"cmd", "ctrl"}
 
--- what to do when an udpate is checked
-function updates.available(available)
-  if available then
-    notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
-  else
-    hydra.alert("No update available.")
-  end
-end
+hotkey.bind(cmdctrl, "l", movewindow_leftside)
+hotkey.bind(cmdctrl, "r", movewindow_rightside)
+
+-- Grid hotkeys
+hotkey.bind(mash, ';', function() ext.grid.snap(window.focusedwindow()) end)
+hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), ext.grid.snap) end)
+
+hotkey.bind(mash, '=', function() ext.grid.adjustwidth( 1) end)
+hotkey.bind(mash, '-', function() ext.grid.adjustwidth(-1) end)
+
+hotkey.bind(mashshift, 'H', function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(mashshift, 'L', function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(mashshift, 'K', function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(mashshift, 'J', function() window.focusedwindow():focuswindow_south() end)
+
+hotkey.bind(mash, 'M', ext.grid.maximize_window)
+
+hotkey.bind(mash, 'N', ext.grid.pushwindow_nextscreen)
+hotkey.bind(mash, 'P', ext.grid.pushwindow_prevscreen)
+
+hotkey.bind(mash, 'J', ext.grid.pushwindow_down)
+hotkey.bind(mash, 'K', ext.grid.pushwindow_up)
+hotkey.bind(mash, 'H', ext.grid.pushwindow_left)
+hotkey.bind(mash, 'L', ext.grid.pushwindow_right)
+
+hotkey.bind(mash, 'U', ext.grid.resizewindow_taller)
+hotkey.bind(mash, 'O', ext.grid.resizewindow_wider)
+hotkey.bind(mash, 'I', ext.grid.resizewindow_thinner)
 
 function movewindow_leftside()
   local win = window.focusedwindow()
@@ -55,17 +59,4 @@ function movewindow_rightside()
   newframe.w = newframe.w / 2
   newframe.x = newframe.w
   win:setframe(newframe)
-end
-
--- Uncomment this if you want Hydra to make sure it launches at login
-autolaunch.set(true)
-
--- check for updates every week
-timer.new(timer.weeks(1), checkforupdates):start()
-notify.register("showupdate", showupdate)
-
--- if this is your first time running Hydra, you're launching it more than a week later, check now
-local lastcheckedupdates = settings.get('lastcheckedupdates')
-if lastcheckedupdates == nil or lastcheckedupdates <= os.time() - timer.days(7) then
-  checkforupdates()
 end
