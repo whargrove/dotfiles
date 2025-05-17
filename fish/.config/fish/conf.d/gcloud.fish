c8a566d54767e4bff42c4c6f5614e907a3e77562
Wes Hargrove <wes@hargrove.me>
2025-05-16 20:17:31 -0600

    Extract gcloud

---
+++ fish/.config/fish/conf.d/gcloud.fish
@@ -0,0 +1,4 @@
+# Add Google Cloud SDK to PATH
+if test -f "$HOME/google-cloud-sdk/path.fish.inc"
+    source "$HOME/google-cloud-sdk/path.fish.inc"
+end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,9 +10,6 @@ fish_add_path $PNPM_HOME
 
 set -gx SSH_AUTH_SOCK ~/.1password/agent.sock
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '$HOME/google-cloud-sdk/path.fish.inc' ]; . '$HOME/google-cloud-sdk/path.fish.inc'; end
-
 # starship
 #starship init fish | source
 
